66dd6284c6e89944773360e29efc6145df5c25c4
christyzkv <christinaphamvuong@gmail.com>
2025-10-17 17:42:44 -0600

    appendand list

--- assignment.py
+++ assignment.py
@@ -1,28 +1,28 @@
 print("Welcome to the Grade Registry Program")
-user_response = input("Would you like to add a new student? y(yes), n(no)")
-while user_response not in ('y', 'n'):
+students=[]
+user_response = input("Would you like to add a new student? y(yes), n(no)").lower()
+while user_response not in ('yes','no', 'n','y'):
             print("Invalid input. Please type 'y' or 'n'.")
-            user_response = input("Would you like to add another student? y(yes), n(no): ")
-while user_response == 'y':
-        if user_response == 'y':
+            user_response = input("Would you like to add another student? y(yes), n(no): ").lower()
+while user_response == ('yes'or 'y' or 'no' or 'n'):
+        if user_response == ('yes' or 'y'):
             student_name = input("Enter the student's name:")
             print("Enter student GPA for each subject. Enter -1 to stop entering GPA.")
             count = 0
             total = 0 
             gpa = float(input())
-            while 4 >= gpa >=0:
+            while gpa != -1:
                 total+=gpa
                 count+=1
                 gpa=float(input())
             if count >0:
                 student_gpa=(total/count)
-                print(student_name, student_gpa)
-            else:
-                  print("GPA is invalid")
-        user_response = input("Would you like to add another student? y(yes), n(no): ")
-
-        #hi guys can u guys see this
-        x=3
-        y=2
-        z=1
-        print("Hi guys!")
+                students.append((student_name, student_gpa))
+        user_response = input("Would you like to add another student? y(yes), n(no): ").lower()
+        while user_response not in ('yes','no','n','y'):
+            print("Invalid input. Please type 'y' or 'n'.")
+            user_response = input("Would you like to add another student? y(yes), n(no): ").lower()
+print("This is the list of students in the system, and their corresponding accumulative GPA")
+for student in students:
+      student_name, student_gpa = student
+      print(student_name,student_gpa)
